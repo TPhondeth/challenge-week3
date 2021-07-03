ed390001a9daf62d1e61f98a27b8efd575ca8a61
Tony Phondeth <phondeth@gmail.com>
2021-07-03 17:51:27 -0400

    created functions

--- script.js
+++ script.js
@@ -16,7 +16,30 @@ let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let number  = "0123456789";
 let symbol = "!#$%&()*+-/<=>?@[]{}_";
 
-// Functions for random character types
+// Function for random lower case
+function randomLowerCase() {
+    return lowerCase.charAt(Math.round(Math.random) * lowerCase.length);
+}
+
+// Function for random upper case
+function randomUpperCase() {
+    return upperCase.charAt(Math.round(Math.random) * upperCase.length);
+}
+
+// Function for random number
+function randomNumber() {
+    return number.charAt(Math.round(Math.random) * number.length);
+}
+
+// Function for random symbol
+function randomSymbol() {
+    return symbol.charAt(Math.round(Math.random) * symbol.length);
+}
+
+// Function to generate password
+function generatePassword() {
+    let length = window.prompt("Choose a password of at least 8 characters and no more than 128 characters.");
+}
 
 
 // Add event listener to generate button
