8f609b7052cf2e93aec5d89dea4c258d793a8341
Tony Phondeth <phondeth@gmail.com>
2021-07-03 23:16:57 -0400

    conditional statements

--- script.js
+++ script.js
@@ -10,35 +10,48 @@ function writePassword() {
 
 }
 
-// Variables for character types
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const number  = "0123456789";
-const symbol = "!#$%&()*+-/<=>?@[]{}_";
-
-// Define Function randomLowerCase
-function randomLowerCase() {
-    return lowerCase.charAt(Math.round(Math.random) * lowerCase.length);
+// Function to get random lower case
+function getRandomLower() {
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    return lowerCase[Math.round(Math.random() * lowerCase.length)];
+}
+// Function to get random upper case
+function getRandomUpper() {
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return upperCase[Math.round(Math.random() * upperCase.length)];
+}
+// Function to get random number
+function getRandomNumber() {
+    const number  = "0123456789";
+    return number[Math.round(Math.random() * number.length)];
 }
 
-// Define Function randomUpperCase
-function randomUpperCase() {
-    return upperCase.charAt(Math.round(Math.random) * upperCase.length);
+// Function to get random symbol
+function getRandomSymbol() {
+    const symbols = "!#$%&()*+-/<=>?@[]{}_";
+    return symbols[Math.round(Math.random() * symbols.length)];
 }
 
-// Define Function randomNumber
-function randomNumber() {
-    return number.charAt(Math.round(Math.random) * number.length);
-}
-
-// Define Function randomSymbol
-function randomSymbol() {
-    return symbol.charAt(Math.round(Math.random) * symbol.length);
-}
-
-// Define Function generatePassword
+// Function to generate password
 function generatePassword() {
-    let length = window.prompt("Choose a password of at least 8 characters and no more than 128 characters.");
+    let pw = prompt("Choose a password of at least 8 characters and no more than 128 characters.");
+
+    // Validate minimum length of password
+    if (pw === "" || pw.length < 8) {
+        alert("Password length must be at least 8 characters.");
+    }
+    // Validate maximum length of password
+    if (pw.length > 128) {
+        alert("Password length must not exceed 128 characters.");
+    }
+    else {
+        const hasLower = confirm("Would you like a Lower Case character?");
+        const hasUpper = confirm("Would you like an Upper Case character?");
+        const hasNumber = confirm("Would you like a Number?");
+        const hasSymbol = confirm("Would you like a Symbol?");
+
+    return passwordGenerator(hasLower, hasUpper, hasNumber, hasSymbol);
+    }
 }
 
 
